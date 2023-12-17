9b559e2ee9dc1815775fa7d0e61b5c636ef04d66
RaMadaSilva <raul01mateia@gmail.com>
2023-12-17 07:13:48 +0100

    iniciando  o merge

--- Challenge.Ibge.Blazor.Domain/Interfaces/ILocalityRepository.cs
+++ Challenge.Ibge.Blazor.Domain/Interfaces/ILocalityRepository.cs
@@ -6,8 +6,12 @@ namespace Challenge.Ibge.Blazor.Domain.Interfaces
     {
         Task CreateAsync(Locality locality);
         Task<IEnumerable<Locality>> GetAllAsync();
-        Task<Locality> GetByIdAsync(long id); 
+        Task<Locality> GetByIdAsync(long id);
         Task RemoveAsync(Locality locality);
+        Task<Locality?> UpdateIbge(Locality locality); 
+        Task<IEnumerable<Locality?>> GetIbgeByCity(string city);
+        Task<IEnumerable<Locality?>> GetIbgeByState(string state);
+        Task<Locality?> GetIbgeById(string id);
 
     }
 }
